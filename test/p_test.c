1e820fd8b7f50a2b88559d894c7dae3a8bb5023c
99002464 <71481589+99002464@users.noreply.github.com>
2020-09-27 20:19:22 +0530

    Update p_test.c

--- test/p_test.c
+++ test/p_test.c
@@ -11,23 +11,23 @@ void tearDown(){}
 
 void test_calculator_add(void)
 {
-TEST_ASSERT_EQUAL(9,calculator(4,5,0));
-  TEST_ASSERT_EQUAL(1,calculator(6,-5,0));
-  TEST_ASSERT_EQUAL(100,calculator(45,55,0));
+TEST_ASSERT_EQUAL(4,calculator(1,3,0));
+  TEST_ASSERT_EQUAL(1,calculator(10,-9,0));
+  TEST_ASSERT_EQUAL(10000,calculator(4500,5500,0));
     TEST_ASSERT_EQUAL(0,matrix(0));
   
 }
 void test_calculator_sub(void)
 {
-  TEST_ASSERT_EQUAL(7,calculator(9,2,1));
-    TEST_ASSERT_EQUAL(43,calculator(45,2,1));
-    TEST_ASSERT_EQUAL(-7,calculator(58,65,1));
+  TEST_ASSERT_EQUAL(4,calculator(9,5,1));
+    TEST_ASSERT_EQUAL(44,calculator(100,56,1));
+    TEST_ASSERT_EQUAL(-20,calculator(100,-120,1));
   
 }
 void test_multi(void)
 {
-  TEST_ASSERT_EQUAL(-49,calculator(-7,7,2));
-    TEST_ASSERT_EQUAL(72,calculator(9,8,2));
+  TEST_ASSERT_EQUAL(-70,calculator(-7,10,2));
+    TEST_ASSERT_EQUAL(1600,calculator(50,32,2));
       TEST_ASSERT_EQUAL(150,calculator(10,15,2));
       TEST_ASSERT_EQUAL(250,calculator(5,50,2));
   
